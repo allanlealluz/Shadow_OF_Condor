4a4f9ac95558c0dc95a2aa218b72c3a5ab3ea36a
allanlealluz <allanlealluz@gmail.com>
2024-04-19 14:56:59 -0300

    changes 19/04 2

--- index.php
+++ index.php
@@ -49,32 +49,38 @@ $fichas = $con->getFichas();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sombra do Condor</title>
     <link rel="shortcut icon" href="sombra_condor.png">
+    <link rel="stylesheet" href="index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
-<h1 style="text-align:center;">Sombra do Condor</h1>
+<body class="bg-dark">
+<div class="container-fluid">
+<h1 style="text-align:center;color:red;padding-bottom:2rem;">Sombra do Condor</h1>
+</div>
+<div class="container bg-light ">
 <form method="post" action="">
-    <label>Nome:</label>
-    <input type="text" name="nome" required>
-    <label>Vitalidade:</label>
-    <input type="number" name="vitalidade" required>
-    <label>Força:</label>
-    <input type="number" name="forca" required>
-    <label>Agilidade:</label>
-    <input type="number" name="agi" required>
-    <label>Inteligência:</label>
-    <input type="number" name="int" required>
-    <label>Presença:</label>
-    <input type="number" name="pre" required>
-    <label>Vigor:</label>
-    <input type="number" name="vigor" required>
-    <label>Itens:</label>
-    <input type="text" name="itens" required>
-    <label>Perícia:</label>
-    <input type="text" name="pericia" required>
-    <label>Defesa:</label>
-    <input type="number" name="defesa" required>
+<div class="row g-3 align-items-center">
+    <label class="form-label">Nome:</label>
+    <input class="form-control" type="text" name="nome" required>
+    <label class="form-label">Vitalidade:</label>
+    <input class="form-control" type="number" name="vitalidade" required>
+    <label class="form-label">Força:</label>
+    <input class="form-control"type="number" name="forca" required>
+    <label class="form-label">Agilidade:</label>
+    <input class="form-control" type="number" name="agi" required>
+    <label class="form-label">Inteligência:</label>
+    <input class="form-control"type="number" name="int" required>
+    <label class="form-label" >Presença:</label>
+    <input class="form-control" type="number" name="pre" required>
+    <label class="form-label">Vigor:</label>
+    <input class="form-control" type="number" name="vigor" required>
+    <label class="form-label">Itens:</label>
+    <input class="form-control"type="text" name="itens" required>
+    <label class="form-label">Perícia:</label>
+    <input class="form-control"type="text" name="pericia" required>
+    <label class="form-label">Defesa:</label>
+    <input class="form-control" type="number" name="defesa" required>
     <input type="submit" name="submit" value="Adicionar">
+    <div>
 </form>
 <table class="table">
     <tr>
@@ -115,29 +121,38 @@ $fichas = $con->getFichas();
 <?php if(isset($_GET['edit'])): ?>
 <h2>Editar Ficha</h2>
 <form method="post" action="">
+<div class="mb-3">
     <input class="form-control" type="hidden" name="id" value="<?php echo $ficha['id']; ?>">
-    <label>Nome:</label>
+    <label class="form-label">Nome:</label>
     <input class="form-control" type="text" name="nome" value="<?php echo $ficha['nome']; ?>" required>
-    <label>Vitalidade:</label>
+    <label class="form-label">Vitalidade:</label>
+</div>
     <input class="form-control" type="number" name="vitalidade" value="<?php echo $ficha['vitalidade']; ?>" required>
-    <label>Força:</label>
+    <label class="form-label">Força:</label>
     <input class="form-control" type="number" name="forca" value="<?php echo $ficha['forca']; ?>" required>
-    <label>Agilidade:</label>
+    <label class="form-label">Agilidade:</label>
+<div class="mb-3">
     <input class="form-control" type="number" name="agi" value="<?php echo $ficha['agi']; ?>" required>
-    <label>Inteligência:</label>
+    <label class="form-label" >Inteligência:</label>
     <input class="form-control" type="number" name="int" value="<?php echo $ficha['inte']; ?>" required>
-    <label>Precisão:</label>
+    <label class="form-label">Presença:</label>
+</div>
+<div class="mb-3">
     <input class="form-control"type="number" name="pre" value="<?php echo $ficha['pre']; ?>" required>
     <label>Vigor:</label>
     <input class="form-control"type="number" name="vigor" value="<?php echo $ficha['vigor']; ?>" required>
     <label>Itens:</label>
+</div>
+<div class="mb-3">
     <input class="form-control"type="text" name="itens" value="<?php echo $ficha['itens']; ?>" required>
     <label>Perícia:</label>
     <input class="form-control"type="text" name="pericia" value="<?php echo $ficha['pericias']; ?>" required>
     <label>Defesa:</label>
     <input class="form-control"type="number" name="defesa" value="<?php echo $ficha['defesa']; ?>" required>
+</div>
     <input class="form-control"type="submit" name="update" value="Atualizar">
 </form>
+</div>
 <?php endif; ?>
 
 </body>
