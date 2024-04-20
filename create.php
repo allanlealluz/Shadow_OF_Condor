d7b0b6d07765fc05e059a6289a852715d17d7919
allanlealluz <allanlealluz@gmail.com>
2024-04-20 20:07:22 -0300

    changes 20/04 1

--- create.php
+++ create.php
@@ -81,41 +81,6 @@ if(isset($_POST['update'])){
     <input type="submit" name="submit" value="Adicionar">
     <div>
 </form>
-<?php if(isset($_GET['edit'])): ?>
-<h2>Editar Ficha</h2>
-<form method="post" action="">
-<div class="mb-3">
-    <input class="form-control" type="hidden" name="id" value="<?php echo $ficha['id']; ?>">
-    <label class="form-label">Nome:</label>
-    <input class="form-control" type="text" name="nome" value="<?php echo $ficha['nome']; ?>" required>
-    <label class="form-label">Vitalidade:</label>
-</div>
-    <input class="form-control" type="number" name="vitalidade" value="<?php echo $ficha['vitalidade']; ?>" required>
-    <label class="form-label">Força:</label>
-    <input class="form-control" type="number" name="forca" value="<?php echo $ficha['forca']; ?>" required>
-    <label class="form-label">Agilidade:</label>
-<div class="mb-3">
-    <input class="form-control" type="number" name="agi" value="<?php echo $ficha['agi']; ?>" required>
-    <label class="form-label" >Inteligência:</label>
-    <input class="form-control" type="number" name="int" value="<?php echo $ficha['inte']; ?>" required>
-    <label class="form-label">Presença:</label>
-</div>
-<div class="mb-3">
-    <input class="form-control"type="number" name="pre" value="<?php echo $ficha['pre']; ?>" required>
-    <label>Vigor:</label>
-    <input class="form-control"type="number" name="vigor" value="<?php echo $ficha['vigor']; ?>" required>
-    <label>Itens:</label>
-</div>
-<div class="mb-3">
-    <input class="form-control"type="text" name="itens" value="<?php echo $ficha['itens']; ?>" required>
-    <label>Perícia:</label>
-    <input class="form-control"type="text" name="pericia" value="<?php echo $ficha['pericias']; ?>" required>
-    <label>Defesa:</label>
-    <input class="form-control"type="number" name="defesa" value="<?php echo $ficha['defesa']; ?>" required>
-</div>
-    <input class="form-control"type="submit" name="update" value="Atualizar">
-</form>
-</div>
-<?php endif; ?>
+
 </body>
 </html>
