a26a8ed32d56f5837a7dd7fb2961186c2532df0b
allanlealluz <allanlealluz@gmail.com>
2024-05-14 19:52:23 -0300

    changes 14/05 3

--- IndexChat.php
+++ IndexChat.php
@@ -12,62 +12,17 @@ require __DIR__ . '/vendor/autoload.php';
 </head>
 <body class='bg-danger'>
     <div class="container bg-dark">
-        <table class="table" id="messages">
+        <h2 class="text-light text-center">Sombra da Condor</h2>
+        <table class="table text-center" id="messages">
         <?php include('DB/chatController.php');?>
         </table>
         <form method="POST">
              <input type="text" id='names' name="name" class="form-control" required>
              <textarea name="message" id='message' class="form-control" required></textarea>
-             <input type="submit" value="Enviar" class="btn btn-primary"  onclick="transmitMessage()">
+             <input type="submit" value="Enviar" class="btn btn-primary" id="btn" onclick="transmitMessage()">
         </form>
 
-<script>
-  const form = document.querySelector('form');
-
-  form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const name = document.getElementById('names').value;
-    const message = document.getElementById('message').value;
-    fetch(`sendMessage.php?name=${name}&message=${message}`);
-    const table = document.getElementById('messages');
-    var tr = document.createElement('tr');
-    var td = document.createElement('td'); 
-    var td2 = document.createElement('td');
-    var corpoTabela = table.getElementsByTagName("tbody")[0];
-    td.innerHTML = name;
-    td2.innerHTML = message;
-    tr.appendChild(td);
-    tr.appendChild(td2);
-    tr.setAttribute('class','bg-light')
-    corpoTabela.appendChild(tr);
-  });
-  var socket  = new WebSocket('ws://localhost:8080');
-
-// Define the 
-var message = document.getElementById('message');
-var names = document.getElementById('names');
-
-function transmitMessage(){
-    socket.send([names.value,',',message.value]);
-}
-
-socket.onmessage = function(e) {
-    const table = document.querySelector('table');
-    var tr = document.createElement('tr');
-    var td = document.createElement('td'); 
-    var td2 = document.createElement('td'); // create a new paragraph element
-    var corpoTabela = table.getElementsByTagName("tbody")[0];
-    data = e.data.split(",");
-    td.textContent = data[0]; 
-    td2.textContent = data[3]; 
-    console.log(data)
-    tr.setAttribute('class','bg-light')
-    tr.appendChild(td);
-    tr.appendChild(td2);// set the text content of the paragraph element to the message received from the socket
-    corpoTabela.appendChild(tr);
-}
-    // Prevent the form from submitting and refreshing the page
-</script>
+<script src="indexChat.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     </div>
 </body>
