6d3169ceeb0d56e11840d4629564388b7250d5f0
allanlealluz <allanlealluz@gmail.com>
2024-05-14 18:38:44 -0300

    changes 14/05 2

--- IndexChat.php
+++ IndexChat.php
@@ -33,12 +33,13 @@ require __DIR__ . '/vendor/autoload.php';
     var tr = document.createElement('tr');
     var td = document.createElement('td'); 
     var td2 = document.createElement('td');
+    var corpoTabela = table.getElementsByTagName("tbody")[0];
     td.innerHTML = name;
     td2.innerHTML = message;
     tr.appendChild(td);
     tr.appendChild(td2);
     tr.setAttribute('class','bg-light')
-    table.appendChild(tr);
+    corpoTabela.appendChild(tr);
   });
   var socket  = new WebSocket('ws://localhost:8080');
 
@@ -55,6 +56,7 @@ socket.onmessage = function(e) {
     var tr = document.createElement('tr');
     var td = document.createElement('td'); 
     var td2 = document.createElement('td'); // create a new paragraph element
+    var corpoTabela = table.getElementsByTagName("tbody")[0];
     data = e.data.split(",");
     td.textContent = data[0]; 
     td2.textContent = data[3]; 
@@ -62,7 +64,7 @@ socket.onmessage = function(e) {
     tr.setAttribute('class','bg-light')
     tr.appendChild(td);
     tr.appendChild(td2);// set the text content of the paragraph element to the message received from the socket
-    table.appendChild(tr);
+    corpoTabela.appendChild(tr);
 }
     // Prevent the form from submitting and refreshing the page
 </script>
