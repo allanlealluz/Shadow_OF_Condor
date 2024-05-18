759e446c4cae13de36f56b23ddc3cc736fac8c4b
allanlealluz <allanlealluz@gmail.com>
2024-05-18 19:24:41 -0300

    changes 18/05 1

--- IndexChat.php
+++ IndexChat.php
@@ -2,7 +2,7 @@
 require __DIR__ . '/vendor/autoload.php';
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
