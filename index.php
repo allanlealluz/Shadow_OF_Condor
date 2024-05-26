6089c27d42da762cfd1f9cf7ec9b3b4f2f836b47
allanlealluz <allanlealluz@gmail.com>
2024-05-25 21:02:10 -0300

    changes 25/05

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $con = new conect;
 $fichas = $con->getFichas();
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
