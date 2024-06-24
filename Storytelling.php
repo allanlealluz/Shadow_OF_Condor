8930ff97aa907f38513d323a7be9a8a6cc6b3bbe
allanlealluz <allanlealluz@gmail.com>
2024-06-24 16:57:25 -0300

    changes 24/06 2

--- Storytelling.php
+++ Storytelling.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet" href="index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body class="bg-dark">
-<div class="container-fluid d-flex justify-content-between">
+<body class="bg-secondary">
+<div class="container-fluid d-flex justify-content-between bg-dark">
     <h1 style="text-align:center;color:red;padding-bottom:2rem;">Sombra do Condor</h1>
     <a href="create.php"><h3 style="text-align:left;color:red;">Create</h3></a>
     <a href="indexChat.php"><h3 style="text-align:left;color:red;">Chat</h3></a>
@@ -23,7 +23,7 @@
     foreach($data as $v){
         ?>
         <a href="Conteudo.php?id=<?php echo $v['id']; ?>">
-            <h2 class='text-primary'><?php echo $v['titulo']; ?></h2>
+            <h2 class='text-primary h2 text-center'><?php echo $v['titulo']; ?></h2>
         </a>
         <?php
     }
@@ -34,7 +34,7 @@
     <form method="post">
         <input class='form-control' type="text" name="titulo" id="titulo">
         <textarea class='form-control' name="conteudo" id="conteudo"></textarea>
-        <input type="submit" value="Send">
+        <input type="submit" value="Send" class="btn btn-secondary d-flex justify-content-center">
         <?php     
         if(isset($_POST['titulo']) && isset($_POST['conteudo'])){
             $titulo = htmlentities($_POST['titulo']);
